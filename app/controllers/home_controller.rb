4fa3764cff176c835553090e81149d0e8d93f5a0
Gustavo Castro <goo@gustavocastro.com>
2011-03-10 20:09:37 -0300

    finished UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     client = Instagram.client(:access_token => "895827.f59def8.68edfeb4b2d94ca59b90ecde732e7eff")
 
     @image = client.user_recent_media.first.images.standard_resolution.url
+    #@image = 'nietzsche.png'
     
   end
 
