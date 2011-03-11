bd40cc0f7eb9e482b8db577009c5254e226ef5c6
Gustavo Castro <goo@gustavocastro.com>
2011-03-11 07:44:24 -0300

    adding animated instagram pics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    client = Instagram.client(:access_token => "895827.f59def8.68edfeb4b2d94ca59b90ecde732e7eff")
-
-    @image = client.user_recent_media.first.images.standard_resolution.url
-    #@image = 'nietzsche.png'
     
   end
 
