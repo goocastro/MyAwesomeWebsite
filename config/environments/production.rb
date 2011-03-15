20b143f3c8290932da3b987a17cec37b654827b0
Gustavo Castro <goo@gustavocastro.com>
2011-03-15 20:20:57 -0300

    commiting sass stuffs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 GustavocastroCom::Application.configure do
+  Sass::Plugin.options[:never_update] = true
+  
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
