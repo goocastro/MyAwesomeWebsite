7b71436d27f71a5df362a5b6432d73ae23655465
Gustavo Castro <goo@gustavocastro.com>
2011-03-24 20:00:18 -0300

    removing rails files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GustavocastroCom::Application.config.secret_token = '3e03fc6416586098f69d6d077ab79c9abe24283d6e68fc547a5cfb469587e83ee0c81406ce3bcc70d407e7df4dc347ce310137642b76e34f92856a96e1563c41'
