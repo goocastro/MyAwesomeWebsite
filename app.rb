a1a9a60a3b5619d5b8778f9306eed2e58a1ac19b
Gustavo Castro <goo@gustavocastro.com>
2011-03-24 21:55:41 -0300

    yap, much better with sinatra... almost done

--- app.rb
+++ app.rb
@@ -5,14 +5,8 @@ require 'instagram'
 
 
 get '/' do
-
-  @image1 = client.user_recent_media.first.images.standard_resolution.url
-  @image2 = client.user_recent_media[2].images.standard_resolution.url
-  @image3 = client.user_recent_media[3].images.standard_resolution.url
-  @image4 = client.user_recent_media[4].images.standard_resolution.url
-  @image5 = client.user_recent_media[5].images.standard_resolution.url
-  @image6 = client.user_recent_media[6].images.standard_resolution.url
-  @image7 = client.user_recent_media[7].images.standard_resolution.url  
+  client = Instagram.client(:access_token => "895827.f59def8.68edfeb4b2d94ca59b90ecde732e7eff")
+  @images = client.user_recent_media
 
   haml :index
 end
