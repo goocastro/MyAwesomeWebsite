b5ce48e4c910f37722454c864d1d66fcf8c294ec
goocastro <goo@isso.ai>
2015-10-17 20:11:37 -0300

    nothing to see here

--- Gemfile
+++ Gemfile
@@ -1,10 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'therubyracer-heroku', :group => :production
 gem 'haml'
-gem 'instagram'
-gem 'json'
 gem 'coffee-script'
-gem 'multi_json'
-gem 'execjs'
--- app.rb
+++ app.rb
@@ -1,63 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-require 'instagram'
-require 'json'
-require 'yaml'
 require 'coffee-script'
-require 'net/http'
-
-images = []
-offset = 0
 
 get '/' do
-  images = getInstagrams
-  @images = images['data']
-  offset = 0
-  @offset = offset
   haml :index
-  #{ :key1 => 'value1', :key2 => 'value2' }.to_json
-end
-
-get '/app.js' do
-  coffee :app
-end
-
-get '/more' do
-  @images = getInstagrams(params['mid'])['data']
-  haml :_list, :layout => false
-end
-
-############################################
-##### get instagram array
-############################################
-def getInstagrams(maxid = '')
-  if maxid == ''
-    cache_filename = 'tmp/instagram_cache.yaml'
-  else
-    cache_filename = "tmp/instagram_cache_#{maxid}.yaml"
-  end
-  pic_list = []
-
-  if !File.exist?(cache_filename.to_s)
-    File.open( cache_filename , 'w') {|f| f.write('') }
-  end
-
-  #if true
-  if File.ctime( cache_filename ) - Time.now < -30 || File.size( cache_filename ) < 5
-    puts "=> getting data from from api"
-    url = "https://api.instagram.com/v1/users/895827/media/recent/?client_id=195fdfcae4844c3b8553e894236f5ada&count=20&max_id=" + maxid
-    p url
-    resp = Net::HTTP.get_response(URI.parse(url))
-    pic_list = JSON.parse(resp.body)
-
-    File.open( cache_filename , "w+" ) do |out|
-      out << pic_list.to_yaml
-    end
-  else
-    puts "=> getting data from file"
-    pic_list = YAML.load(File.open( cache_filename ))
-  end
-
-  return pic_list
 end
